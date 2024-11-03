fc3a96c21e569934a3c91b1852ed55182727e3e5
Benny Chen <benny.jw.chen@gmail.com>
2024-11-02 19:09:04 -0700

    add sign in for github

---
+++ app/components/signin.tsx
@@ -0,0 +1,19 @@
+/** Sign in with Github component */
+import { signIn } from "@/auth"
+ 
+export default function SignIn() {
+  return (
+    <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <form
+            action={async () => {
+                "use server"
+                await signIn("github")
+            }}
+        >
+      <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" type="submit">Signin with GitHub</button>
+    </form>
+
+    </div>
+       
+  )
+} 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 /** Landing page */
-import Login from '@/app/ui/login';
+import Login from '@/app/components/login';
+import { auth } from '@/auth'
+import SignIn from './components/signin';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        <h1 className="text-2xl">Ultimate Training App</h1>
       
-      <Login></Login>  
+      <SignIn></SignIn>
       
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
