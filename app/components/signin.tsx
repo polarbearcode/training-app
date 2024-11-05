bc2a2c038d8eb552650e55c1149145da2baa00ab
Benny Chen <benny.jw.chen@gmail.com>
2024-11-04 17:25:30 -0800

    git log in component

--- app/components/signin.tsx
+++ app/components/signin.tsx
@@ -13,7 +13,6 @@ export default function SignIn() {
       <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" type="submit">Signin with GitHub</button>
     </form>
 
-    </div>
-       
+    </div>    
   )
 } 
