6594b677cbbd50b8ee52b97873ccff195536ecc5
Benny Chen <benny.jw.chen@gmail.com>
2024-11-06 22:26:24 -0800

    page for credentials login

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+/** login page when the user hits login (for Credentials) */
+
+import CredentialsSignInComp from "../components/CredentialsSignInComp";
+
+export default function CredentialsLogInPage() {
+    return (
+        <>
+        <CredentialsSignInComp></CredentialsSignInComp>
+        </>
+        
+        
+    )
+}
