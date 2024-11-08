3fdc42d0a615ee369fa173c7dc909ebc9d284649
Benny Chen <benny.jw.chen@gmail.com>
2024-11-08 14:49:29 -0800

    styling

--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -4,47 +4,51 @@
 import { useActionState } from "react";
 import { UserState, registerUser } from "../lib/actions";
 
-export default function signUpForm() {
+export default function SignUpForm() {
     const initialState: UserState = {message: null, errors: {}};
     const [state, formAction] = useActionState(registerUser, initialState);
-
     return (
         <>
-            <div>
+          
+            <div className="grid h-16 place-items-center min-h-screen bg-blue-50">
                 <h1>SIGN UP</h1>
-            </div>
-            <form action={formAction}>
-            {/* User's name */}
-            <div>
-                <label htmlFor="name">Name</label>
-                <input
-                    id="name"
-                    name="name"
-                    type="string"
-                />
-            </div>
+                <form action={formAction}>
+                    {/* User's name */}
+                    <div className="mb-4">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="string"
+                            className="ml-2"
+                        />
+                    </div>
 
-            {/* User email */}
-            <div>
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="string"
-                />
-            </div>
+                    {/* User email */}
+                    <div className="mb-4">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="string"
+                            className="ml-2"
+                        />
+                    </div>
 
-              {/* User password*/}
-              <div>
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    type="string"
-                />
+                    {/* User password*/}
+                    <div className="mb-4">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="string"
+                            className="ml-2"
+                        />
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
             </div>
-        </form>
- 
+            
         </>
     )
 }
