1bbd2e31efdd3a92383a7d3735f2f687fe14f98c
Benny Chen <benny.jw.chen@gmail.com>
2024-11-08 14:02:21 -0800

    action function for registering

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,17 +1,30 @@
+'use server';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
+import { db } from '@vercel/postgres';
+import { z } from 'zod';
+import bcrypt  from 'bcryptjs';
 
-export async function authenticate( // called in the login form CredentialsSignIn
+const client = await db.connect() // connect to database
+
+const UserFormSchema = z.object({
+    email: z.string(),
+    password: z.string()
+})
+
+export async function authenticate( // called in the login form CredentialsSignInComp
     prevState: string | undefined,
     formData: FormData,
   ) {
     try {
-      await signIn('credentials', formData); // from NextAuth export in auth.ts
+    
+        /** Sign in using the data from the form */
+      await signIn('credentials', formData); // signIn function from NextAuth export in auth.ts
     } catch (error) {
       if (error instanceof AuthError) {
         switch (error.type) {
           case 'CredentialsSignin':
-            return 'Invalid credentials.';
+            return 'Invalid credentials';
           default:
             return 'Something went wrong.';
         }
@@ -19,3 +32,36 @@ export async function authenticate( // called in the login form CredentialsSignI
       throw error;
     }
   }
+ 
+  /**
+   * Register a user and insert into user db
+   * @param prevState prev state of the form
+   * @param formData  FormData containing name, email, etc.
+   */
+export async function registerUser(
+    prevState: string | undefined,
+    formData: FormData
+){
+    try {
+        const validatedFields = UserFormSchema.safeParse({
+            email : formData.get('email'),
+            password: formData.get('password')
+        });
+
+
+        if (validatedFields.success) {
+            const email = validatedFields.data?.email;
+            const password = validatedFields.data?.password;
+             const hashedPassword = bcrypt.hash(password, 10);
+            await client.sql`
+                INSERT INTO training_users (email, password)
+                VALUES (${email}, ${password})
+                ON CONFLICT (email) DO NOTHING;
+            `
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
