8e110955014a30e8562719cc70312b8f6f0c8b3c
Benny Chen <benny.jw.chen@gmail.com>
2024-11-08 14:20:32 -0800

    fixed returns for useaction

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,10 +7,21 @@ import bcrypt  from 'bcryptjs';
 
 const client = await db.connect() // connect to database
 
+/** Validation for registration form */
 const UserFormSchema = z.object({
     email: z.string(),
     password: z.string()
 })
+
+/** State for login and signup forms */
+export type UserState = {
+    errors?: {
+        email?: string[];
+        password?: string[];
+    }, 
+
+    message?: string | null 
+}
 
 export async function authenticate( // called in the login form CredentialsSignInComp
     prevState: string | undefined,
@@ -34,34 +45,39 @@ export async function authenticate( // called in the login form CredentialsSignI
   }
  
   /**
-   * Register a user and insert into user db
+   * Register a user and insert into training_user db
    * @param prevState prev state of the form
    * @param formData  FormData containing name, email, etc.
    */
 export async function registerUser(
-    prevState: string | undefined,
+    prevState: UserState,
     formData: FormData
 ){
-    try {
-        const validatedFields = UserFormSchema.safeParse({
-            email : formData.get('email'),
-            password: formData.get('password')
-        });
+
+    const validatedFields = UserFormSchema.safeParse({
+        email : formData.get('email'),
+        password: formData.get('password')
+    });
 
 
-        if (validatedFields.success) {
-            const email = validatedFields.data?.email;
-            const password = validatedFields.data?.password;
-             const hashedPassword = bcrypt.hash(password, 10);
+    if (validatedFields.success) {
+        const email = validatedFields.data?.email;
+        const password = validatedFields.data?.password;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        try {
             await client.sql`
-                INSERT INTO training_users (email, password)
-                VALUES (${email}, ${password})
-                ON CONFLICT (email) DO NOTHING;
+            INSERT INTO training_users (email, password)
+            VALUES (${email}, ${hashedPassword})
+            ON CONFLICT (email) DO NOTHING;
             `
+        return {};
+        } catch (error) {
+            return {message: 'User could not be created'};
         }
-
-    } catch (error) {
-        console.log(error);
+    } else {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missig fields'
+        }
     }
-
 }
