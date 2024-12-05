ffb658d9fd08375de0d73633cf8f6d6163148c08
Benny Chen <benny.jw.chen@gmail.com>
2024-12-04 16:21:24 -0800

    Display user info once logged in

---
+++ app/components/userInfo.tsx
@@ -0,0 +1,15 @@
+/* User information to display when user is logged in. */
+
+'use client'
+
+import { useSession } from "next-auth/react";
+
+export default function UserInfo() {
+
+    const { data: session, status } = useSession(); 
+
+    return (
+        <p>Hello {session?.user?.name}</p>
+    )
+
+}
