c2483410d595a429b6881021baa02cc29d0fd1a7
Benny Chen <benny.jw.chen@gmail.com>
2024-12-04 16:25:16 -0800

    move user info display to another comp

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,19 +1,27 @@
-import { signOut } from "@/auth";
+
+import UserInfo from "app/components/userInfo";
+import { signOut } from "../../auth";
 import { PowerIcon } from "@heroicons/react/16/solid";
 
+
 /** User profile page when they log in */
-export default function ProfileHomePage() {
+export default async function ProfileHomePage() {
+
     return (
-        <form
-        action={async () => {
-            'use server';
-            await signOut();
-        }}
-        >
-        <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-        </button>
-        </form>
+        <>
+            
+            <UserInfo></UserInfo>
+            <form
+            action={async () => {
+                "use server"
+                await signOut();
+            }}
+            >
+            <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                <PowerIcon className="w-6" />
+                <div className="hidden md:block">Sign Out</div>
+            </button>
+            </form>
+        </>
     )
 }
