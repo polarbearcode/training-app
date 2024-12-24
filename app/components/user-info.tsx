3c559bf4aacdbb4dd33fa482acff1cccc40cba11
Benny Chen <benny.jw.chen@gmail.com>
2024-12-23 22:37:24 -0800

    reformatting

--- app/components/user-info.tsx
+++ app/components/user-info.tsx
@@ -2,15 +2,22 @@
 
 'use client'
 
+import { auth } from "../../auth";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function UserInfo() {
 
-    const { data: session, status } = useSession(); 
+    const { data: session, status} = useSession(); 
+ 
 
     return (
         <>
+            
             <p>Hello {session?.user?.name}</p>
+           
+            
+          
            
         </>
        
