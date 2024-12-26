aff15adaa79f7e663107e0ccd7ab14e258eebec9
Benny Chen <benny.jw.chen@gmail.com>
2024-12-26 14:25:28 -0800

    new routes

---
+++ app/profile/add/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return (
+        <p>Form to add a workout</p>
+    )
+}
---
+++ app/profile/training/page.tsx
@@ -0,0 +1,14 @@
+/** Weekly breakout of workouts
+ * | week 1 >|
+ * | week 2 >|
+ * | ...... >|
+ * 
+ */
+export default function page() {
+    return (
+        <div>
+            <li>Week 1</li>
+            <li>Week 2</li>
+        </div>
+    )
+}
