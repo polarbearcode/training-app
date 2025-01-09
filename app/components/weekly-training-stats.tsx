f428eb4069ec5a44ab86763a46b0f5a1b47ba169
Benny Chen <benny.jw.chen@gmail.com>
2025-01-08 20:54:47 -0800

    put button on side and add stats

--- app/components/weekly-training-stats.tsx
+++ app/components/weekly-training-stats.tsx
@@ -5,11 +5,15 @@
 
 import { DatabaseActivity } from "app/lib/definitions"
 import { useState } from "react";
+import { ArrowDownCircleIcon } from "@heroicons/react/16/solid";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
+import { analyzeRunType } from "app/lib/ai/run-analysis";
 
-export default function WeekTrainingStats({activityList, beginDate, endDate} : {
+export default function WeekTrainingStats({activityList, beginDate, endDate, weekNum} : {
     activityList: DatabaseActivity[];
     beginDate: Date;
     endDate: Date;
+    weekNum: number
 }) {
 
     // for toggling showing stats
@@ -32,6 +36,8 @@ export default function WeekTrainingStats({activityList, beginDate, endDate} : {
     // process stats
     let totalRuns = 0;
     let totalDistance = 0;
+    let totalStrengthTraining  = 0;
+    let typeOfRunCount: Record<string, number> = {"Long Run": 0, "Hills": 0, "Easy": 0};
 
     for (let i = 0; i < activitiesWithinDate.length; i++) {
 
@@ -40,7 +46,19 @@ export default function WeekTrainingStats({activityList, beginDate, endDate} : {
 
         if (curActivity.activitytype === "Run") {
             totalRuns++;
+
+            const runTags: string[] = analyzeRunType(curActivity.totalelevationgain, curActivity.distance, curActivity.averagespeed);
+
+            for (let j = 0; j < runTags.length; j++) {
+                typeOfRunCount[runTags[j]] = typeOfRunCount[runTags[j]] + 1;
+            }
         }
+
+        if (curActivity.activitytype === "Workout") {
+            totalStrengthTraining++;
+        }
+
+
     }
 
     function toggleHide() {
@@ -52,13 +70,24 @@ export default function WeekTrainingStats({activityList, beginDate, endDate} : {
 
     return (
         <>
-            <button onClick={toggleHide}>Hide</button>
-            {!hide && 
+        <div className="grid grid-cols-2 p-4">
+            <h1>Week {weekNum}: </h1>
+            <button onClick={toggleHide} className="border-solid border-2 bg-green-50 p-2 h-10">
+                {hide? "Show" : "Hide"}
+            </button>
+        </div>
+    
+        {!hide && 
                 <ul>
-                    <li>{totalDistance}</li>
-                    <li>{totalRuns}</li>
+                    <li className="text-center">Total Distance: {totalDistance}</li>
+                    <li className="text-center">Total Runs: {totalRuns}</li>
+                    <li className="text-center">Total Long Runs: {typeOfRunCount["Long Run"]}</li>
+                    <li className="text-center">Total Hill Runs: {typeOfRunCount["Hills"]}</li>
+                    <li className="text-center">Total Easy Runs: {typeOfRunCount["Easy"]}</li>
                 </ul>
             }
+
+            
         </>
     )
 }
