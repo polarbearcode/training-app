497baa7b46289fe13c022f50e009704a283c1394
Benny Chen <benny.jw.chen@gmail.com>
2025-01-11 12:45:44 -0800

    moved response status text check to be right after response

--- app/api/profile/callback/route.ts
+++ app/api/profile/callback/route.ts
@@ -50,6 +50,10 @@ export async function GET(req: NextRequest) {
         },
       });
 
+      if (response.statusText != 'OK') {
+        return NextResponse.json({ error: 'Failed to exchange code for access token' }, { status: 500 });
+      }   
+
     
       const access_token = response.data.access_token; // user access token
       const athlete_id = response.data.athlete.id;
@@ -63,17 +67,9 @@ export async function GET(req: NextRequest) {
         return NextResponse.json({ error: 'Unable to get activities or unable to put them to database' }, { status: 500 });
       } */
 
-
-      
-
-      if (response.statusText != 'OK') {
-        return NextResponse.json({ error: 'Failed to exchange code for access token' }, { status: 500 });
-      }      
-
+  
 
       return NextResponse.redirect(baseURL, {status:307});
-
-    
 
 
       //return NextResponse.json({ message: 'Exchange success' }, { status: 302 });
