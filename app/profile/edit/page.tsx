02fe4c4d419b341a9f1fcf2c50ecde8d4c275325
Benny Chen <benny.jw.chen@gmail.com>
2025-01-22 22:18:34 -0800

    simplify

--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -1,25 +1,12 @@
 
 import UpdateUserForm from "app/components/user-profile/update-user-info-form"
-import { getUserProfile } from "app/lib/actions/actions";
-import { getActivitesFromDB } from "app/lib/actions/strava-activities"
-import { UpdateFormState, updateUserProfile } from "app/lib/actions/updateFormActions";
-import { auth } from "auth";
-import { useSession } from "next-auth/react";
-import { useState, useActionState, useEffect, Suspense } from "react";
-
 
 
 /** Form to add user profile data
  * Marathon goal time, last data fetch from Strava (hidden)
  */
 export default async function page() {
-    
-    const session = await auth();
-    
-    if (session == null) {
-        return <p>Loading</p>
-    }
-    
+        
     return (
       
             <UpdateUserForm></UpdateUserForm>
