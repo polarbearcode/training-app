6c41646de6f3ec6a41ef17d22eea24dea1a47f7b
Benny Chen <benny.jw.chen@gmail.com>
2025-01-22 22:49:32 -0800

    move stuff from training page to here

---
+++ app/components/training-tabs.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useState } from "react";
+import WorkOutDisplay from "./workout-display";
+import WeeklyTrainingTable from "./training-weeks";
+import { DatabaseActivity } from "app/lib/definitions";
+
+/** Used in the training/page.tsx */
+
+export default function TrainingTabs({email, activityList, userTrainingStartDate} : {
+    email: string;
+    activityList: DatabaseActivity[];
+    userTrainingStartDate: Date;
+}) {
+    
+    const [currentTab, setCurrentTab] = useState("Workouts");
+
+    return (
+        <>
+            <div id="selection-tab" className="grid grid-cols-2 mb-10">
+                <button onClick={() => setCurrentTab("Workouts")} className={`${currentTab === "Workouts" ? 'bg-orange-300' : ''}`}>
+                    Workouts
+                </button>
+                <button onClick={() => setCurrentTab("Training")} className={`${currentTab === "Training" ? 'bg-orange-300' : ''}`}>
+                     Weekly Training
+                </button>
+            </div>
+            {currentTab === "Workouts" && <WorkOutDisplay activityList={activityList}></WorkOutDisplay>}
+            {currentTab === "Training" && <WeeklyTrainingTable activityList={activityList} 
+                userTrainingStartDate={userTrainingStartDate}
+                email={email}/>
+            }
+        
+        </>
+    )
+}
