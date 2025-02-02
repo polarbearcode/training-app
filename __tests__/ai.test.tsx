08cb6c4829ad630e192b70aeaa3a84673cd7c065
Benny Chen <benny.jw.chen@gmail.com>
2025-02-01 18:08:43 -0800

    test for edge cases of paces

--- __tests__/ai.test.tsx
+++ __tests__/ai.test.tsx
@@ -7,12 +7,12 @@ test("Get run tags", () => {
 
 test("Get run percentages", () => {
 
-    const dummyLaps = {'Lap 1': {"distance": 1609.34, "average_speed": 5}, //9
-    'Lap 2': {"distance": 1609.34, "average_speed": 4}, 'Lap 3': {"distance": 1609.34, "average_speed": 4.1,},
-    'Lap 4': {"distance": 1609.34, "average_speed": 5.5,}, 'Lap 5': {"distance": 1609.34, "average_speed": 4.5,},
-    'Lap 6': {distance: 1609.34, "average_speed": 3.5}, 'Lap 7': {distance: 1609.34, "average_speed": 3.5},
-    'Lap 8': {distance: 1609.34, "average_speed": 3}, 'Lap 9': {distance: 1609.34, "average_speed": 3},
-    'Lap 10': {distance: 1609.34, "average_speed": 2.5}}
+    const dummyLaps = {'Lap 1': {"distance": 1609.34, "average_speed": 2.82}, //9
+    'Lap 2': {"distance": 1609.34, "average_speed": 2.99}, 'Lap 3': {"distance": 1609.34, "average_speed": 3.16,},
+    'Lap 4': {"distance": 1609.34, "average_speed": 3.36,}, 'Lap 5': {"distance": 1609.34, "average_speed": 3.58,},
+    'Lap 6': {distance: 1609.34, "average_speed": 3.84}, 'Lap 7': {distance: 1609.34, "average_speed": 2.98},
+    'Lap 8': {distance: 1609.34, "average_speed": 3.15}, 'Lap 9': {distance: 1609.34, "average_speed": 3.35},
+    'Lap 10': {distance: 1609.34, "average_speed": 3.57}}
 
 
     const activity1: DatabaseActivity = {activityid: '1', athleteid: '1', activitytype: "Run", year: 2025, month: 1, day: 2, distance: 16093.4, 
@@ -27,7 +27,11 @@ test("Get run percentages", () => {
         totalelevationgain: 0, averagespeed: 0, averagecadence: 0, maxspeed: 0, averageheartrate: 0, 
         email: 'dummy@example.com', laps: JSON.parse(JSON.stringify(dummyLaps))};
 
-    const expectedPercentages = {easy: 0.1, aerobic: 0.4, mp: 0.2, hm: 0.1, '5K': 0.1, '10K': 0.1};
+    const expectedPercentages = {easy: 0.2, aerobic: 0.2, mp: 0.2, hm: 0.2, '5K': 0.1, '10K': 0.1};
+    const actualPercentages = analyzeRunPercentages([activity1, activity2, activity3], 8.5);
 
-    expect(analyzeRunPercentages([activity1, activity2, activity3], 4)).toEqual(expectedPercentages);
+    Object.entries(expectedPercentages).forEach(([key, value]) => {
+        expect(Math.round(actualPercentages[key as keyof typeof actualPercentages] * 100) / 100).toBe(expectedPercentages[key as keyof typeof expectedPercentages]);
+    })
+
 })
