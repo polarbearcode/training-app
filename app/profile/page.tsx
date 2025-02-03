39ec130f1baa355bdd9250a269592383e4c84ba5
Benny Chen <benny.jw.chen@gmail.com>
2025-02-03 15:15:30 -0800

    change to use server, remove import button

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -17,14 +17,10 @@ export default async function ProfileHomePage() {
         <> 
 
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <div>
-                
-                        Hello {session?.user?.name}
-              
-                   
+                <div>          
+                    Hello {session?.user?.name}             
                 </div>
 
-               
             </div> 
 
             
